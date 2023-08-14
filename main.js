15f1ae8480ffb1a54831c15a9d13b101de67cfee
Liran Tal <liran.tal@gmail.com>
2023-08-14 21:08:10 +0300

    feat: add error checking for config specific state required to use the integration
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import { fileURLToPath } from "url";
 import packageMeta from "./package.json";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const pluginName = packageMeta.name;
+const astroIntegrationName = packageMeta.name;
 
 export default function triggerdevAstroPlugin(options) {
   return {
-    name: pluginName,
+    name: astroIntegrationName,
     hooks: {
       "astro:config:setup": ({
         config: astroConfig,
@@ -15,6 +15,12 @@ export default function triggerdevAstroPlugin(options) {
         injectRoute,
         updateConfig,
       }) => {
+        if (config.output === "static") {
+          throw new Error(
+            `[${astroIntegrationName}] \`output: "server"\` or \`output: "hybrid"\` is required to use this Astro integration.`
+          );
+        }
+
         injectRoute({
           pattern: "/api/trigger",
           entryPoint: join(__dirname, "./route.js"),
