d2dc95881a06244c66c0a0dca8678968739f75f5
Liran Tal <liran.tal@gmail.com>
2023-08-14 21:05:50 +0300

    feat: use package name programatically from package.json file
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import packageMeta from "./package.json";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const pluginName = "triggerdev-astro-integration";
+const pluginName = packageMeta.name;
 
 export default function triggerdevAstroPlugin(options) {
   return {
