6f5e6913ce82ec9003fe489cff405339d83d88ea
Liran Tal <liran.tal@gmail.com>
2023-08-16 14:09:10 +0300

    fix: re-position error to the hook after configuration has completed
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- main.js
+++ main.js
@@ -9,22 +9,19 @@ export default function triggerdevAstroIntegration(options) {
   return {
     name: astroIntegrationName,
     hooks: {
-      "astro:config:setup": ({
-        config: astroConfig,
-        injectScript,
-        injectRoute,
-        updateConfig,
-      }) => {
-        if (astroConfig.output === "static") {
-          throw new Error(
-            `[${astroIntegrationName}] \`output: "server"\` or \`output: "hybrid"\` is required to use this Astro integration.`
-          );
-        }
-
+      "astro:config:setup": ({ injectRoute }) => {
         injectRoute({
           pattern: "/api/trigger",
           entryPoint: join(__dirname, "./route.js"),
         });
+      },
+
+      "astro:config:done": ({ config }) => {
+        if (config.output === "static") {
+          throw new Error(
+            `[${astroIntegrationName}] \`output: "server"\` or \`output: "hybrid"\` is required to use this Astro integration.`
+          );
+        }
       },
     },
   };
--- route.js
+++ route.js
@@ -1,4 +1,4 @@
-export const get = async () => {
+export const get = async (ctx) => {
   return {
     status: 200,
     body: "Response from Trigger.dev plugin!",
